4edf90bd702e306012e9cc0c96ed32e9d1e636dc
tnewman <tnewman@localhost>
2013-08-03 15:16:11 -0400

    Defined interfaces

--- GPIO Driver/gpio.c
+++ GPIO Driver/gpio.c
@@ -5,8 +5,31 @@
  * Date:        tnewman
  */
 
-#include "config.h"
+#include "gpio.h"
 #include "register.h"
 
+static volatile void* gpio_memory = 0x00;
 
+/*
+ * Name:        check_root
+ * Description: Verifies that the executable is running as root or is seteuid root.
+ * Inputs:      None
+ * Returns:     SUCCESS or NOT_ROOT.
+ */
+StatusCode check_root();
 
+/*
+ * Name:        check_system
+ * Description: Verifies that the executable is running on a Raspberry Pi with a known board revision.
+ * Inputs:      None
+ * Returns:     SUCCESS, INVALID_CHIPSET, or INVALID_REVISION
+ */
+StatusCode check_system();
+
+/*
+ * Name:        check_pin
+ * Description: Verifies that the pin number supplied is a valid pin number for the given pin type.
+ * Inputs:      pin number to check, pin type to check against
+ * Returns:     SUCCESS, INVALID_TYPE, or INVALID_PIN_NUMBER
+ */
+StatusCode check_pin(int pin_number, PinNumType pin_type);
--- GPIO Driver/gpio.h
+++ GPIO Driver/gpio.h
@@ -1,13 +1,74 @@
 /*
  * File:        gpio.h
- * Description: Declaration of GPIO functions.
+ * Description: Declaration of GPIO functions. This Header file serves as the API for the GPIO library.
  * Programmer:  Aug 1, 2013
  * Date:        tnewman
+ */
+
+/*
+ * API Usage
+ * map_gpio_memory to initialize (must return success to proceed)
+ * set_pin to set a GPIO Pin
+ * clear_pin to clear a GPIO Pin
+ * unmap_gpio_memory to clean up (do not set or clear after calling this)
  */
 
 #ifndef GPIO_H_
 #define GPIO_H_
 
-#include "register.h"
+/*
+ * PinNumType specifies the numbering convention of the GPIO pins.
+ */
+typedef enum {
+	BROADCOM, // The pin numbers in the Broadcom manual referenced by the Raspberry Pi's schematic.
+	P1CONNECTOR // The physical pin numbers on the Raspberry Pi's P1 connector.
+} PinNumType;
+
+/*
+ * StatusCode specifies the codes that functions can return.
+ */
+typedef enum {
+	SUCCESS, // The function returned successfully.
+	NOT_ROOT, // The executable is not seteuid root, and the executable was not run as root (either one will work).
+	INVALID_CHIPSET, // The hardware is not a Raspberry Pi.
+	INVALID_REVISION, // The hardware revision is not recognized by the program.
+	CANNOT_MAP_MEMORY, // The GPIO memory cannot be mapped for an unknown reason (sorry, can't do any better).
+	MEMORY_NOT_MAPPED, // The GPIO memory region has not been mapped yet
+	INVALID_TYPE, // The pin type is not any of the allowed pin types.
+	INVALID_PIN_NUMBER, // The pin number is not a pin number supported by the current pin type.
+} StatusCode;
+
+/*
+ * Name:        map_gpio_memory
+ * Description: Maps the GPIO memory region. The memory will not be mapped again if it has already been
+ *              allocated.
+ * Inputs:      None
+ * Returns:     SUCCESS or CANNOT_MAP_MEMORY
+ */
+StatusCode map_gpio_memory();
+
+/*
+ * Name:        set_pin
+ * Description: Sets the specified GPIO pin.
+ * Inputs:      pin number and pin numbering type
+ * Returns:     SUCCESS, MEMORY_NOT_MAPPED, INVALID_TYPE, or INVALID_PIN_NUMBER
+ */
+StatusCode set_pin(int pin_number, PinNumType pin_type);
+
+/*
+ * Name:        clear_pin
+ * Description: Clears the specified GPIO pin.
+ * Inputs:      pin number and pin numbering type
+ * Returns:     SUCCES, MEMORY_NOT_MAPPED, INVALID_TYPE, or INVALID_PIN_NUMBER
+ */
+StatusCode clear_pin(int pin_number, PinNumType pin_type);
+
+/*
+ * Name:        unmap_gpio_memory
+ * Description: Unmaps the GPIO memory region. Does nothing if no memory is mapped.
+ * Inputs:      None
+ * Returns:     SUCCESS
+ */
+StatusCode unmap_gpio_memory();
 
 #endif /* GPIO_H_ */
